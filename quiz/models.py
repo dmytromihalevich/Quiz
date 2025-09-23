55a302bde8250cccfd89f4446e6124ef5653814c
dmytromihalevich <mihalevicmaria502@gmail.com>
2025-09-23 19:29:00 +0300

    start project and create models

---
+++ quiz/models.py
@@ -0,0 +1,39 @@
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    time_per_question = models.IntegerField(default=30)  # сек
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    text = models.TextField()
+    image = models.ImageField(upload_to='questions/', blank=True, null=True)
+    video_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.text[:50]
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.quiz.title} - {self.score}"
